bb0cef972e80b211ee45935b6a25346943d6aa16
Juan Mestanza <jcmlmph@gmail.com>
2025-01-31 17:59:00 -0500

    si es

--- packages/data_repository/lib/src/utils/env.dart
+++ packages/data_repository/lib/src/utils/env.dart
@@ -1,4 +1,4 @@
 class Environment {
-  static String host = 'http://119.8.150.201:8480';
+  static String host = 'http://119.8.150.201:8480' ;
   static String hostDWH = 'https://matricesderiesgo.grupoflesan.com/api/validateEmail';
 }
